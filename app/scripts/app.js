16117e7a985f9de6bee00b8166a509fa453db3da
Luis Macias <luismacias.angulo@gmail.com>
2016-11-30 23:51:42 -0600

    Primer commit, escenario con el index visible
    Signed-off-by: Luis Macias <luismacias.angulo@gmail.com>

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ocoApp
+ * @description
+ * # ocoApp
+ *
+ * Main module of the application.
+ */
+angular
+	.module('ocoApp', [
+		'ngAnimate'
+	]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ocoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ocoApp
+ */
+angular.module('ocoApp')
+	.controller('MainCtrl', function () {
+		this.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+	});
